db437d776b3e2fa2633d8d1b95de344694195960
Param Singh Makwana <paramsinghmakwana@gmail.com>
2024-11-11 14:21:03 +0000

    add metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,28 @@ export default function RootLayout({
 }) {
 	return (
 		<html lang="en">
-			<link rel="icon" href="/babel.svg" type="image/x-icon"></link>
+			<head>
+				{/* Basic Meta Tags */}
+				<meta property="og:title" content="Babel" />
+				<meta property="og:description" content="Blogging Platform" />
+				<meta
+					property="og:image"
+					content="https://opengraph.githubassets.com/21b294e69066486315ba191aa24d0493ab6a074c423a36ba5411b5fd410ea30c/Kratospidey/gbs"
+				/>
+
+				{/* WhatsApp specific */}
+				<meta property="og:image:width" content="1200" />
+				<meta property="og:image:height" content="630" />
+
+				{/* Discord specific */}
+				<meta name="twitter:card" content="Babel" />
+				<meta
+					name="twitter:image"
+					content="https://opengraph.githubassets.com/21b294e69066486315ba191aa24d0493ab6a074c423a36ba5411b5fd410ea30c/Kratospidey/gbs"
+				/>
+
+				<link rel="icon" href="/babel.svg" type="image/x-icon"></link>
+			</head>
 			<body className={inter.className}>
 				<RootLayoutClient>
 					<Navbar />
