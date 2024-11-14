028898b40f2e2b1848dc15ed4fa15a486112e02d
Param Singh Makwana <paramsinghmakwana@gmail.com>
2024-11-14 09:41:27 +0000

    change gradient

--- src/components/ui/background-gradient-animation.tsx
+++ src/components/ui/background-gradient-animation.tsx
@@ -1,6 +1,5 @@
-// src/components/ui/BackgroundGradientAnimation.tsx
 "use client";
-import React, { useEffect, useRef } from "react";
+import React from "react";
 import { cn } from "@/lib/utils";
 
 export const BackgroundGradientAnimation = ({
@@ -8,35 +7,8 @@ export const BackgroundGradientAnimation = ({
 }: {
 	className?: string;
 }) => {
-	const containerRef = useRef<HTMLDivElement>(null);
-
-	useEffect(() => {
-		const handleMouseMove = (e: MouseEvent) => {
-			if (containerRef.current) {
-				const rect = containerRef.current.getBoundingClientRect();
-				const x = e.clientX - rect.left;
-				const y = e.clientY - rect.top;
-				const xPercent = (x / rect.width) * 100;
-				const yPercent = (y / rect.height) * 100;
-				containerRef.current.style.setProperty("--mouse-x", `${xPercent}%`);
-				containerRef.current.style.setProperty("--mouse-y", `${yPercent}%`);
-			}
-		};
-		if (containerRef.current) {
-			containerRef.current.addEventListener("mousemove", handleMouseMove);
-		}
-		return () => {
-			if (containerRef.current) {
-				containerRef.current.removeEventListener("mousemove", handleMouseMove);
-			}
-		};
-	}, []);
-
 	return (
-		<div
-			className={cn("relative w-full h-full overflow-hidden", className)}
-			ref={containerRef}
-		>
+		<div className={cn("relative w-full h-full overflow-hidden", className)}>
 			<div className="absolute inset-0 gradient-animation"></div>
 		</div>
 	);
